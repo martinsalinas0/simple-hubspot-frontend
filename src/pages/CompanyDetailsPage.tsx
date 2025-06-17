ac3504d2406e6ec6e14f8058945475fa817481eb
martinsalinas0 <martinlsalinas@pm.me>
2025-06-16 23:37:41 -0500

    add CompanyDetailsPage component to display company information

---
+++ src/pages/CompanyDetailsPage.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import type { AppDispatch, RootState } from "../stores/configureStore";
+import { getCompById } from "../stores/slices/companiesSclice";
+import CompanyDetailsComp from "../components/CompanyDetails";
+
+const CompanyDetailsPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const company = useSelector(
+    (state: RootState) => state.companies.currentCompany
+  );
+  const isLoading = useSelector(
+    (state: RootState) => state.companies.isLoading
+  );
+  const error = useSelector((state: RootState) => state.companies.error);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getCompById(id));
+    }
+  }, [dispatch, id]);
+
+  if (isLoading) return <p className="text-center mt-10">Loading company...</p>;
+  if (error)
+    return <p className="text-center mt-10 text-red-600">Error: {error}</p>;
+  if (!company) return <p className="text-center mt-10">No company found.</p>;
+
+  return (
+    <div className="flex justify-center items-center min-h-screen p-6 bg-gray-100">
+      <CompanyDetailsComp
+        name={company.name}
+        location={company.location}
+        _id={company._id}
+        status={company.status}
+        logoURL={company.logoURL}
+      />
+    </div>
+  );
+};
+
+export default CompanyDetailsPage;
