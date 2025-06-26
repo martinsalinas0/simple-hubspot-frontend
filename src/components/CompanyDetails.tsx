29f78e78c174fb728789d4ba38c59ef1fbe9389b
martinsalinas0 <martinlsalinas@pm.me>
2025-06-26 00:31:47 -0500

    feat: enforce dealAmount as a required field in CompanyProps; format deal amount display as currency in CompanyDetails component

--- src/components/CompanyDetails.tsx
+++ src/components/CompanyDetails.tsx
@@ -11,7 +11,7 @@ interface CompanyProps {
   pointOfContact?: string;
   phoneNumber?: string;
   customerEmail?: string;
-  dealAmount?: number;
+  dealAmount: number;
 }
 
 const CompanyDetailsComp: React.FC<CompanyProps> = ({
@@ -90,11 +90,12 @@ const CompanyDetailsComp: React.FC<CompanyProps> = ({
       <hr className="my-3" />
 
       <p className="mb-2 text-lg">
-        <strong className="text-gray-700 ">
-          Deal Amount: <span className="font-serif">$</span>
-        </strong>
-        {dealAmount}
-        .00 <p>Current Deal:</p>
+        <strong className="text-gray-700 ">Deal Amount: </strong>
+        {dealAmount.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })}
+        <p>Current Deal:</p>
       </p>
       <hr className="my-3" />
 
@@ -106,26 +107,6 @@ const CompanyDetailsComp: React.FC<CompanyProps> = ({
       {/* status label */}
       <p className="mb-4 text-lg">
         <strong className="text-gray-700">Status:</strong> <em>{status}</em>{" "}
-        <div className="relative inline-block text-left">
-          <button
-            id="dropdown-button"
-            className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Options
-            <svg
-              className="w-5 h-5 ml-2 -mr-1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
       </p>
       <p>Point of Contact: {pointOfContact}</p>
       <p>Phone Number: {phoneNumber}</p>
